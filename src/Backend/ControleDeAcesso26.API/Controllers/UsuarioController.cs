acfbbb6de79c610534a38b7f58a96f90d53ddc07
Henrique Figueiredo <henricklima94@gmail.com>
2026-02-04 19:23:57 -0300

    feat: camada API - UsuarioController (recuperar e criar usuário), controller base do projeto, program.cs e demais configurações

---
+++ src/Backend/ControleDeAcesso26.API/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using ControleDeAcesso26.API.Controllers.Base;
+using ControleDeAcesso26.Application.UseCases.UsuarioUseCases.Interfaces;
+using ControleDeAcesso26.Communication.Requests.RequestsUsuario;
+using ControleDeAcesso26.Communication.Responses.ResponsesUsuario;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeAcesso26.API.Controllers
+{
+    public class UsuarioController : ControleDeAcesso26ControllerBase
+    {
+        [HttpGet(nameof(RecuperarUsuarios))]
+        [ProducesResponseType(typeof(ResponseRecuperarUsuariosJson), StatusCodes.Status200OK)]
+        public async Task<IActionResult> RecuperarUsuarios([FromServices] IRecuperarUsuariosUseCase recuperaUsuariosUseCase, 
+                                                           [FromQuery] bool incluirInativos = false)
+        {
+            var listaDeUsuariosResult = await recuperaUsuariosUseCase.Execute(incluirInativos);
+            return Ok(listaDeUsuariosResult);
+        }
+
+        [HttpPost(nameof(CriarUsuario))]
+        [ProducesResponseType(typeof(ResponseCriarUsuarioJson), StatusCodes.Status201Created)]
+        public async Task<IActionResult> CriarUsuario([FromServices] ICriarUsuarioUseCase criaUsuarioUseCase, 
+                                         [FromBody] RequestCriarUsuarioJson requestCriaUsuario)
+        {
+            var usuarioResult = await criaUsuarioUseCase.Execute(requestCriaUsuario);
+            return Created(string.Empty, usuarioResult);
+        }
+    }
+}
--- src/Backend/ControleDeAcesso26.API/Program.cs
+++ src/Backend/ControleDeAcesso26.API/Program.cs
@@ -1,19 +1,26 @@
+using ControleDeAcesso26.Application.DependencyInjection;
+using ControleDeAcesso26.Infrastructure.DependencyInjection;
+using ControleDeAcesso26.Infrastructure.Migrations;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructureLayer(builder.Configuration);
+builder.Services.AddApplicationLayer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapSwagger("/openapi/{documentName}.json");
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +28,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+DatabaseMigration.MigrateDatabase(builder.Configuration, scope.ServiceProvider);
 
 app.Run();
