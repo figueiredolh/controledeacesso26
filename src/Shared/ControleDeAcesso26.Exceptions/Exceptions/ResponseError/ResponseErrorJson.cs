fb5ecbf73f1c9a8b788e68c870cf0960b25a1d37
Henrique Figueiredo <henricklima94@gmail.com>
2026-02-05 18:05:13 -0300

    feat: classe de resposta para exceção (ResponseErrorJson)

---
+++ src/Shared/ControleDeAcesso26.Exceptions/Exceptions/ResponseError/ResponseErrorJson.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeAcesso26.Exceptions.Exceptions.ResponseError
+{
+    public class ResponseErrorJson
+    {
+        public List<string> ErrorMessages { get; }
+        public ResponseErrorJson(List<string> errorMessages)
+        {
+            ErrorMessages = errorMessages;
+        }
+    }
+}
