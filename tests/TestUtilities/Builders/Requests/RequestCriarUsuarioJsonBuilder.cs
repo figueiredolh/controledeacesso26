67f74c391055b6340d488c6c5aec45917bd17ecd
Henrique Figueiredo <henricklima94@gmail.com>
2026-02-05 23:22:51 -0300

    test: testes de validator para o request para a criação de usuário (+ projeto base) feat: atualização de solução .sln

---
+++ tests/TestUtilities/Builders/Requests/RequestCriarUsuarioJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using ControleDeAcesso26.Communication.Requests.RequestsUsuario;
+
+namespace TestUtilities.Builders.Requests
+{
+    public static class RequestCriarUsuarioJsonBuilder
+    {
+        public static RequestCriarUsuarioJson Build()
+        {
+            return new Faker<RequestCriarUsuarioJson>()
+                .RuleFor(request => request.Nome, (f) => f.Name.FullName())
+                .RuleFor(request => request.Apelido, (f) => f.Internet.UserName());
+        }
+    }
+}
---
+++ tests/Validators.Test/RequestCriarUsuarioValidatorTest.cs
@@ -0,0 +1,50 @@
+﻿using ControleDeAcesso26.Application.UseCases.UsuarioUseCases.Validators;
+using ControleDeAcesso26.Exceptions.ValidatorsRulesResourceMessages;
+using Shouldly;
+using TestUtilities.Builders.Requests;
+
+namespace Validators.Test
+{
+    public class RequestCriarUsuarioValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var request = RequestCriarUsuarioJsonBuilder.Build();
+
+            var validator = new RequestCriarUsuarioValidator();
+            var validationResult = validator.Validate(request);
+
+            validationResult.IsValid.ShouldBeTrue();
+            validationResult.Errors.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        public void FailureOnNomeEmptyProperty()
+        {
+            var request = RequestCriarUsuarioJsonBuilder.Build();
+            request.Nome = "";
+
+            var validator = new RequestCriarUsuarioValidator();
+            var validationResult = validator.Validate(request);
+
+            validationResult.IsValid.ShouldBeFalse();
+            validationResult.Errors.Count.ShouldBe(1);
+            validationResult.Errors.Select(error => error.ErrorMessage).FirstOrDefault().ShouldBe(ValidatorsRulesResourceMessages.USUARIO_NOME_VAZIO);
+        }
+
+        [Fact]
+        public void FailureOnApelidoEmptyProperty()
+        {
+            var request = RequestCriarUsuarioJsonBuilder.Build();
+            request.Apelido = "";
+
+            var validator = new RequestCriarUsuarioValidator();
+            var validationResult = validator.Validate(request);
+
+            validationResult.IsValid.ShouldBeFalse();
+            validationResult.Errors.Count.ShouldBe(1);
+            validationResult.Errors.Select(error => error.ErrorMessage).FirstOrDefault().ShouldBe(ValidatorsRulesResourceMessages.USUARIO_APELIDO_VAZIO);
+        }
+    }
+}
