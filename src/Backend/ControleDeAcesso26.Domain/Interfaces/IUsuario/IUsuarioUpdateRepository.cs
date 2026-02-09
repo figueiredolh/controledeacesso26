bab700adb929564a149647fea01c9f9e13962c90
Henrique Figueiredo <henricklima94@gmail.com>
2026-02-09 15:26:24 -0300

    feat: interface IUsuarioUpdateRepository (+ mudanças em IUsuarioReadOnlyRepository)

--- src/Backend/ControleDeAcesso26.Domain/Interfaces/IUsuario/IUsuarioUpdateRepository.cs
+++ src/Backend/ControleDeAcesso26.Domain/Interfaces/IUsuario/IUsuarioUpdateRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace ControleDeAcesso26.Domain.Interfaces.IUsuario
+﻿using ControleDeAcesso26.Domain.Entities;
+
+namespace ControleDeAcesso26.Domain.Interfaces.IUsuario
 {
     public interface IUsuarioUpdateRepository
     {
+        public void AtualizarUsuario(Usuario usuario);
     }
 }
