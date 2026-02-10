5e3575b68e45210f6e09494b26a48044d580029f
Henrique Figueiredo <henricklima94@gmail.com>
2026-02-10 01:35:36 -0300

    refactor: adicionando parâmetro opcional em IUsuarioReadOnlyRepository

--- src/Backend/ControleDeAcesso26.Domain/Interfaces/IUsuario/IUsuarioReadOnlyRepository.cs
+++ src/Backend/ControleDeAcesso26.Domain/Interfaces/IUsuario/IUsuarioReadOnlyRepository.cs
@@ -5,7 +5,7 @@ namespace ControleDeAcesso26.Domain.Interfaces.IUsuario
     public interface IUsuarioReadOnlyRepository
     {
         public Task<List<Usuario>> RecuperarUsuarios(bool incluirInativos);
-        public Task<Usuario?> RecuperarUsuarioPorId(long id);
+        public Task<Usuario?> RecuperarUsuarioPorId(long id, bool usuarioAtivo = true);
         public Task<bool> ApelidoJaExisteNoSistema(string apelido);
     }
 }
