eaf5655589f74a2737b61ceb3b623f38195c5aa1
Henrique Figueiredo <henricklima94@gmail.com>
2026-02-15 18:38:23 -0300

    test: Teste para validator de atualizar usuário (+ TestUtilities)

---
+++ tests/TestUtilities/Builders/UsuarioBuilders/Entity/UsuarioBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Bogus;
+using ControleDeAcesso26.Domain.Entities;
+
+namespace TestUtilities.Builders.UsuarioBuilders.Entity
+{
+    public static class UsuarioBuilder
+    {
+        public static Usuario Build()
+        {
+            return new Faker<Usuario>()
+                .RuleFor(usuario => usuario.Id, u => u.IndexFaker + 1)
+                .RuleFor(usuario => usuario.DataCriacao, u => DateTime.Now)
+                .RuleFor(usuario => usuario.Ativo, u => true)
+                .RuleFor(usuario => usuario.Nome, u => u.Name.FullName())
+                .RuleFor(usuario => usuario.Apelido, u => u.Internet.UserName());
+        }
+
+        public static Usuario Build(string? nome, string? apelido = null)
+        {
+            string _nome = nome ?? new Faker().Name.FullName();
+            string _apelido = apelido ?? new Faker().Internet.UserName();
+
+            return new Faker<Usuario>()
+                .RuleFor(usuario => usuario.Id, u => u.IndexFaker + 1)
+                .RuleFor(usuario => usuario.DataCriacao, u => DateTime.Now)
+                .RuleFor(usuario => usuario.Ativo, u => true)
+                .RuleFor(usuario => usuario.Nome, () => _nome)
+                .RuleFor(usuario => usuario.Apelido, () => _apelido);
+        }
+    }
+}
---
+++ tests/TestUtilities/Builders/UsuarioBuilders/Requests/RequestAtualizarUsuarioJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using ControleDeAcesso26.Communication.Requests.RequestsUsuario;
+
+namespace TestUtilities.Builders.UsuarioBuilders.Requests
+{
+    public static class RequestAtualizarUsuarioJsonBuilder
+    {
+        public static RequestAtualizarUsuarioJson Build()
+        {
+            return new Faker<RequestAtualizarUsuarioJson>()
+                .RuleFor(request => request.Nome, (f) => f.Name.FullName())
+                .RuleFor(request => request.Apelido, (f) => f.Internet.UserName());
+        }
+    }
+}
