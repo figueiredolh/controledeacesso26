327e4a09cda19df63fd0059dd7e089857ca94d7c
Henrique Figueiredo <henricklima94@gmail.com>
2026-03-02 15:35:51 -0300

    feat: controller para ações de biometria de usuário

--- src/Backend/ControleDeAcesso26.API/Controllers/BiometriaUsuarioController.cs
+++ src/Backend/ControleDeAcesso26.API/Controllers/BiometriaUsuarioController.cs
@@ -1,5 +1,7 @@
 ﻿using ControleDeAcesso26.API.Controllers.Base;
 using ControleDeAcesso26.Application.UseCases.BiometriaUsuarioUseCases.Interfaces;
+using ControleDeAcesso26.Communication.Responses.ResponsesBiometriaUsuario;
+using ControleDeAcesso26.Exceptions.Exceptions.ResponseError;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeAcesso26.API.Controllers
@@ -7,10 +9,12 @@ namespace ControleDeAcesso26.API.Controllers
     public class BiometriaUsuarioController : ControleDeAcesso26ControllerBase
     {
         [HttpPost("{idUsuario}")]
+        [ProducesResponseType(typeof(ResponseCadastrarBiometriaUsuarioJson), StatusCodes.Status200OK)]
+        [ProducesErrorResponseType(typeof(ResponseErrorJson))]
         public async Task<IActionResult> Cadastrar(long idUsuario, [FromServices] ICadastrarBiometriaUsuarioUseCase useCase)
         {
-            var result = useCase.Execute(idUsuario);
-            return Ok();
+            var result = await useCase.Execute(idUsuario);
+            return Ok(result);
         }
     }
 }
