61bc1a0965e1d7b51cd32936f35af60aa689c313
Henrique Figueiredo <henricklima94@gmail.com>
2026-03-03 19:24:18 -0300

    feat: entidade (+ interface) do template de biometria (TemplateBiometriaUsuario)

---
+++ src/Backend/ControleDeAcesso26.Domain/Entities/TemplateBiometriaUsuario.cs
@@ -0,0 +1,12 @@
+﻿using ControleDeAcesso26.Domain.Base;
+
+namespace ControleDeAcesso26.Domain.Entities
+{
+    public class TemplateBiometriaUsuario : EntityBase
+    {
+        public required long IdSensor1 { get; set; }
+        public long? IdSensor2 { get; set; }
+        public required byte[] Template { get; set; }
+        public required long IdUsuario { get; set; }
+    }
+}
---
+++ src/Backend/ControleDeAcesso26.Domain/Interfaces/ITemplateBiometriaUsuario/ITemplateBiometriaReadOnlyRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace ControleDeAcesso26.Domain.Interfaces.ITemplateBiometriaUsuario
+{
+    public interface ITemplateBiometriaReadOnlyRepository
+    {
+        public Task<bool> IdSensor1JaExiste(long idSensor);
+    }
+}
