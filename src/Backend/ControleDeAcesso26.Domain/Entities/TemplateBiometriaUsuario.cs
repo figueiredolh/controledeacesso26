e898e8fcb9141e7631ea44b86f0b33a8a761b279
Henrique Figueiredo <henricklima94@gmail.com>
2026-03-05 12:41:44 -0300

    feat: adicionando propriedade de navegação e foreign key na entidade TemplateBiometriaUsuario

--- src/Backend/ControleDeAcesso26.Domain/Entities/TemplateBiometriaUsuario.cs
+++ src/Backend/ControleDeAcesso26.Domain/Entities/TemplateBiometriaUsuario.cs
@@ -1,4 +1,5 @@
 ﻿using ControleDeAcesso26.Domain.Base;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ControleDeAcesso26.Domain.Entities
 {
@@ -8,5 +9,8 @@ namespace ControleDeAcesso26.Domain.Entities
         public long? IdSensor2 { get; set; }
         public required byte[] Template { get; set; }
         public required long IdUsuario { get; set; }
+
+        [ForeignKey("IdUsuario")]
+        public Usuario Usuario { get; set; } = null!;
     }
 }
