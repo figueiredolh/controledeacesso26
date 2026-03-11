f206b540e5b002aefabc25d70cb5424b1455478e
Henrique Figueiredo <henricklima94@gmail.com>
2026-03-11 17:51:00 -0300

    feat: tópico para feedback da operação em tempo real - cadastro de biometria

--- src/Shared/ControleDeAcesso26.Communication/MQTTCommunication/Topics/MqttTopics.cs
+++ src/Shared/ControleDeAcesso26.Communication/MQTTCommunication/Topics/MqttTopics.cs
@@ -5,6 +5,7 @@
         public const string Teste = "controledeacesso26/teste";
         public const string CadastrarBiometriaUsuarioSensor1 = "controledeacesso26/biometria/cadastro/sensor1";
         public const string CadastrarBiometriaUsuarioEnviarDadosSensor1 = "controledeacesso26/biometria/cadastro/sensor1/enviar";
+        public const string CadastrarBiometriaUsuarioEnviarDadosSensor1Feedback = "controledeacesso26/biometria/cadastro/sensor1/enviar/feedback";
 
         public const string ExcluirBiometriaUsuarioSensor1 = "controledeacesso26/biometria/excluir/sensor1";
         public const string ExcluirBiometriaUsuarioSensor2 = "controledeacesso26/biometria/excluir/sensor2";
