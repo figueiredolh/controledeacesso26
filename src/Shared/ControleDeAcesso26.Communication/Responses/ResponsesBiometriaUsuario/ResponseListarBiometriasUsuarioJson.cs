de93e1b0f6a9f374057ba1b85538418e9953c709
Henrique Figueiredo <henricklima94@gmail.com>
2026-03-12 14:40:34 -0300

    feat: objeto de response para listar biometrias de usuário - ResponseListarBiometrias

---
+++ src/Shared/ControleDeAcesso26.Communication/Responses/ResponsesBiometriaUsuario/ResponseListarBiometriasUsuarioJson.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleDeAcesso26.Communication.Responses.ResponsesBiometriaUsuario
+{
+    public class ResponseListarBiometriasUsuarioJson
+    {
+        public string NomeUsuario { get; set; } = null!;
+        public int IdSensor1 { get; init; }
+        public int? IdSensor2 { get; init; }
+    }
+}
