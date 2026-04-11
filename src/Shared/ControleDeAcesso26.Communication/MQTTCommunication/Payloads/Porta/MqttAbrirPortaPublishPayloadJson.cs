fc2b9f3dbffc9fa48492213610a17e24bd720000
Henrique Figueiredo <henricklima94@gmail.com>
2026-04-10 21:52:39 -0300

    feat: payload de informações ao abrir porta

---
+++ src/Shared/ControleDeAcesso26.Communication/MQTTCommunication/Payloads/Porta/MqttAbrirPortaPublishPayloadJson.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleDeAcesso26.Communication.MQTTCommunication.Payloads.Porta
+{
+    public class MqttAbrirPortaPublishPayloadJson
+    {
+        public required string NomeUsuario { get; init; }
+        public required string Acao { get; init; }
+        public required DateTime Horario { get; init; }
+    }
+}
