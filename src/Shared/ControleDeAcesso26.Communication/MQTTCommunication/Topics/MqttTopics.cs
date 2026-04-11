14aad7a841451895dcb9e0e6a87d3fc0a596fa56
Henrique Figueiredo <henricklima94@gmail.com>
2026-04-10 21:45:48 -0300

    feat: novos tópicos para verificação de biometria e abertura de porta

--- src/Shared/ControleDeAcesso26.Communication/MQTTCommunication/Topics/MqttTopics.cs
+++ src/Shared/ControleDeAcesso26.Communication/MQTTCommunication/Topics/MqttTopics.cs
@@ -2,7 +2,6 @@
 {
     public static class MqttTopics
     {
-        public const string Teste = "controledeacesso26/teste";
         public const string CadastrarBiometriaUsuarioSensor1 = "controledeacesso26/biometria/cadastro/sensor1";
         public const string CadastrarBiometriaUsuarioEnviarDadosSensor1 = "controledeacesso26/biometria/cadastro/sensor1/enviar";
         public const string CadastrarBiometriaUsuarioEnviarDadosSensor1Feedback = "controledeacesso26/biometria/cadastro/sensor1/enviar/feedback";
@@ -11,7 +10,10 @@
         public const string ExcluirBiometriaUsuarioSensor2 = "controledeacesso26/biometria/excluir/sensor2";
         public const string ExcluirBiometriaUsuarioEnviarDadosSensor1 = "controledeacesso26/biometria/excluir/sensor1/enviar";
         public const string ExcluirBiometriaUsuarioEnviarDadosSensor2 = "controledeacesso26/biometria/excluir/sensor2/enviar";
-        //public const string CadastrarRfidUsuario = "controledeacesso26/cadastro/rfid/cadastrardados";
-        //public const string LeituraRfidUsuario = "controledeacesso26/rfid/lerdados";
+
+        public const string VerificarBiometriaUsuarioSensor1 = "controledeacesso26/biometria/verificar/sensor1";
+        public const string VerificarBiometriaUsuarioSensor2 = "controledeacesso26/biometria/verificar/sensor2";
+
+        public const string AbrirPorta = "controledeacesso26/biometria/porta/abrir";
     }
 }
