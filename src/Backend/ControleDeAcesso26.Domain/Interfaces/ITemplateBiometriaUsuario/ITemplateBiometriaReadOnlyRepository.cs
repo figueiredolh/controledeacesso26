0f1da7fa2fad050e7869d6c63c90f6261461e280
Henrique Figueiredo <henricklima94@gmail.com>
2026-04-10 21:55:42 -0300

    feat: método de verificação de usuário ativo em ITemplateBiometriaReadOnlyRepository

--- src/Backend/ControleDeAcesso26.Domain/Interfaces/ITemplateBiometriaUsuario/ITemplateBiometriaReadOnlyRepository.cs
+++ src/Backend/ControleDeAcesso26.Domain/Interfaces/ITemplateBiometriaUsuario/ITemplateBiometriaReadOnlyRepository.cs
@@ -7,5 +7,6 @@ namespace ControleDeAcesso26.Domain.Interfaces.ITemplateBiometriaUsuario
         public Task<List<TemplateBiometriaUsuario>> BuscarTemplates(int? idUsuario = null, int currentPage = 1, int pageSize = 10);
         public Task<bool> IdSensor1JaExiste(int idSensor);
         public Task<TemplateBiometriaUsuario?> BuscarTemplatePorId(int idSensor);
+        public Task<bool> UsuarioAtivo(int idSensor, int sensor = 1);
     }
 }
